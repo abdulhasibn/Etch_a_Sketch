1eb1abd26b8bdb2fe93fa63c09dfbf7733d6ef4c
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-09 11:10:35 +0530

    Add grids to the canvas div
    Found a way to add grids to the canvas using Javascript dynamically.
    First approach was not successful. However, this one went through.

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-const canvas = document.querySelector("#canvas");
-let rows = document.querySelectorAll(".row");
-let cells = document.querySelectorAll(".cell");
+const canvas = document.querySelector(".canvas");
+function makeCells(rows, cols){
+    canvas.style.setProperty(("--grid-rows"), rows);
+    canvas.style.setProperty(("--grid-cols"), cols);
+    for (let i = 0; i < (rows * cols); i ++){
+        let cell = document.createElement("div");
+        canvas.appendChild(cell).className = "grid-item";
+    }
 
-makeGrids();
-
-function makeGrids(){
-    makeRows(16);
-    makeCells(16);
 }
-function makeRows(rowNum){
-    for (let i = 0; i < rowNum; i++){
-        let row = document.createElement('div');
-        canvas.appendChild(row).className = "row";
-        console.log("works");
-
-    };
-};
-
-function makeCells(colNum){
-    for (let i = 0; i < rows.length; i++){
-        for (let j = 0; j < colNum; j++){
-            let col = document.createElement('div');
-            rows[j].appendChild(col).className = "cell";
-            console.log("Works");
-        };
-    };
-};
+makeCells(16,16);
