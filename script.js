b4b3f08dfccd1a8ca874a7f4b63924eeed91449e
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-09 15:32:10 +0530

    Add button features to the page
    This commit makes the black, eraser and reset buttons effective.
    The event that the grids listen to is 'mouseover'.
    I am figuring out a way to make it into click and drag.

--- script.js
+++ script.js
@@ -4,8 +4,35 @@ function makeCells(rows, cols){
     canvas.style.setProperty(("--grid-cols"), cols);
     for (let i = 0; i < (rows * cols); i ++){
         let cell = document.createElement("div");
-        canvas.appendChild(cell).className = "grid-item";
+        canvas.appendChild(cell).className = "grid_item";
     }
+    let displaySize = document.createElement("p");
+    displaySize.innerText = `${rows}x${cols}`;
+    const hero = document.querySelector(".hero");
+    hero.appendChild(displaySize).className = "display_size";
 
 }
-makeCells(16,16);
+makeCells(16, 16);
+const grids = document.getElementsByClassName("grid_item");
+const blackButton =document.getElementById("black");
+blackButton.addEventListener('click', () =>{
+    for (const grid of grids){
+        grid.addEventListener('mouseover',() => {
+            grid.style.backgroundColor = "black"
+        } );
+    }
+});
+const eraserButton = document.getElementById("eraser");
+eraserButton.addEventListener('click', () =>{
+    for (const grid of grids){
+        grid.addEventListener('mouseover',() => {
+            grid.style.backgroundColor = "white"
+        } );
+    }
+});
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+    for (const grid of grids){
+        grid.style.backgroundColor = "white";
+    }
+})
